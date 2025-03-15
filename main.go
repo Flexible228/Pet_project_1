efcddb1f821bbb4b86a0dcd471bfc65c671f77f9
Flexible228 <wayperoppo@gmail.com>
2025-03-16 01:56:14 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -13,20 +13,37 @@ type RequestBody struct {
 var task string
 
 func postHandler(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var body RequestBody
-	err := decoder.Decode(body)
-	if err != nil {
-		panic(err)
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
+
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+
+	var reqBody RequestBody
+	err := decoder.Decode(&reqBody)
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	task = reqBody.Task
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Task resived: %s", task)
 }
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("hello, ", task)
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "Helo, %s", task)
 }
 
 func main() {
 	http.HandleFunc("/post", postHandler)
 	http.HandleFunc("/get", getHandler)
+
 	http.ListenAndServe("localhost:8080", nil)
 }
