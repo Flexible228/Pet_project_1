ec029763069ea534168d10eb7bec27883242e9b6
Flexible228 <wayperoppo@gmail.com>
2025-03-21 01:12:06 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -2,48 +2,45 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-type RequestBody struct {
-	Task string `json:"task"`
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if result := DB.Create(&task); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(task)
 }
 
-var task string
-
-func postHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+func GetTask(w http.ResponseWriter, r *http.Request) {
+	var tasks []Task
+	if result := DB.Find(&tasks); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	decoder := json.NewDecoder(r.Body)
-	defer r.Body.Close()
-
-	var reqBody RequestBody
-	err := decoder.Decode(&reqBody)
-	if err != nil {
-		http.Error(w, "bad request", http.StatusBadRequest)
-		return
-	}
-
-	task = reqBody.Task
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Task resived: %s", task)
-}
-
-func getHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	fmt.Fprintf(w, "Helo, %s", task)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
 }
 
 func main() {
-	http.HandleFunc("/post", postHandler)
-	http.HandleFunc("/get", getHandler)
 
-	http.ListenAndServe("localhost:8080", nil)
+	InitDB()
+
+	DB.AutoMigrate(&Task{})
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/tasks", CreateTask).Methods("POST")
+	router.HandleFunc("/api/tasks", GetTask).Methods("GET")
+	http.ListenAndServe(":8080", router)
 }
