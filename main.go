da78b821c01decaf7a82c04d5aae4e30f6c80f33
Flexible228 <wayperoppo@gmail.com>
2025-03-27 22:06:30 +0300

    Рефакторинг и создание структуры

--- main.go
+++ main.go
@@ -1,99 +1,26 @@
 package main
 
 import (
-	"encoding/json"
+	"awesomeProject/internal/database"
+	"awesomeProject/internal/handlers"
+	"awesomeProject/internal/taskService"
 	"github.com/gorilla/mux"
 	"net/http"
-	"strconv"
 )
 
-func CreateTask(w http.ResponseWriter, r *http.Request) {
-	var task Task
-	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	if result := DB.Create(&task); result.Error != nil {
-		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(task)
-}
-
-func GetTask(w http.ResponseWriter, r *http.Request) {
-	var tasks []Task
-	if result := DB.Find(&tasks); result.Error != nil {
-		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(tasks)
-}
-
-func PatchTask(w http.ResponseWriter, r *http.Request) {
-	var taskUpdates Task
-	if err := json.NewDecoder(r.Body).Decode(&taskUpdates); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
-		return
-	}
-
-	var task Task
-	if result := DB.First(&task, id); result.Error != nil {
-		http.Error(w, "Task not found", http.StatusNotFound)
-		return
-	}
-	
-	if taskUpdates.Task != "" {
-		task.Task = taskUpdates.Task
-	}
-	task.IsDone = taskUpdates.IsDone
-
-	if result := DB.Save(&task); result.Error != nil {
-		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(task)
-}
-
-func DeleteTask(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
-		return
-	}
-
-	result := DB.Delete(&Task{}, id)
-	if result.RowsAffected == 0 {
-		http.Error(w, "Task not found", http.StatusNotFound)
-		return
-	}
-	w.WriteHeader(http.StatusNoContent)
-}
-
 func main() {
+	database.InitDB()
+	database.DB.AutoMigrate(&taskService.Task{})
 
-	InitDB()
+	repo := taskService.NewTaskRepository(database.DB)
+	service := taskService.NewService(repo)
 
-	DB.AutoMigrate(&Task{})
+	handler := handlers.NewHandler(service)
 
 	router := mux.NewRouter()
-	router.HandleFunc("/api/tasks", CreateTask).Methods("POST")
-	router.HandleFunc("/api/tasks", GetTask).Methods("GET")
-	router.HandleFunc("/api/tasks/{id}", DeleteTask).Methods("DELETE")
-	router.HandleFunc("/api/tasks/{id}", PatchTask).Methods("PATCH")
+	router.HandleFunc("/api/task", handler.GetTasksHandler).Methods("GET")
+	router.HandleFunc("/api/task", handler.PostTaskHandler).Methods("POST")
+	router.HandleFunc("/api/task/{id}", handler.PatchTaskHandler).Methods("PATCH")
+	router.HandleFunc("/api/task/{id}", handler.DeleteTaskHandler).Methods("DELETE")
 	http.ListenAndServe(":8080", router)
 }
---
+++ taskHandlers.go
@@ -0,0 +1,90 @@
+package handlers
+
+import (
+	"awesomeProject/internal/taskService"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+)
+
+type Handler struct {
+	Service *taskService.TaskService
+}
+
+// Нужна для создания структуры Handler на этапе инициализации приложения
+
+func NewHandler(service *taskService.TaskService) *Handler {
+	return &Handler{
+		Service: service,
+	}
+}
+
+func (h *Handler) GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	tasks, err := h.Service.GetAllTasks()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func (h *Handler) PostTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task taskService.Task
+	err := json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	createdTask, err := h.Service.CreateTask(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(createdTask)
+}
+
+func (h *Handler) PatchTaskHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "invalid task ID", http.StatusBadRequest)
+		return
+	}
+
+	var task taskService.Task
+	err = json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	updatedTask, err := h.Service.UpdateTaskByID(uint(taskID), task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedTask)
+}
+
+func (h *Handler) DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "invalid task ID", http.StatusBadRequest)
+		return
+	}
+
+	err = h.Service.DeleteTaskByID(uint(taskID))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
